4dd95917a8c138e8c998a450448a837273e5b892
TheRandomHero <deadmaus840203>
2019-02-05 14:28:01 +0100

    background and game cells

--- static/snake.js
+++ static/snake.js
@@ -8,6 +8,7 @@ function drawGame() {
     table.setAttribute('id', 'table');
     container.appendChild(table);
     let gameBoard = document.getElementById('table');
+
     function drawboard(){
         for (let row=0; row < 40; row++){
             let rows = document.createElement('tr');
@@ -16,16 +17,41 @@ function drawGame() {
             gameBoard.appendChild(rows);
             for (let col=0; col<40; col++){
                 let cols = document.createElement('td');
-                cols.setAttribute('class', 'col');
-                cols.setAttribute('id', row + "," + col);
+                cols.setAttribute('class', 'grid');
+                cols.setAttribute('id', col + "," + row);
+                cols.setAttribute('data-coordinate-x', String(col));
+                cols.setAttribute('data-coordinate-y', String(row));
                 rows.appendChild(cols);
 
             }
         }
+        }
+    function drawSnake() {
+        let snake = [{
+            coordinateX: 4,
+            coordinateY: 0
+        },{
+            coordinateX: 3,
+            coordinateY: 0
+        },{
+            coordinateX: 2,
+            coordinateY: 0
+        },{
+            coordinateX: 1,
+            coordinateY: 0
+        }]
+        let headX = snake[0].coordinateX;
+        let headY = snake[0].coordinateY;
+        for (let i=0; i<snake.length; i++){
+            let coordinateX = snake[i].coordinateX;
+            let coordinateY = snake[i].coordinateY;
+            let snakeGrid = document.getElementById(String(coordinateX)+","+ String(coordinateY));
+            snakeGrid.setAttribute('class', 'snake')
 
+            }
     }
-drawboard()
-
+drawboard();
+drawSnake()
 
 
 
